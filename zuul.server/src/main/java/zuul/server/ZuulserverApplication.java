f0da20e31546748ad0a401f80b67cef3515a9dbb
sonusgsits08 <sonu.sgsits08@gmail.com>
2018-12-23 20:54:17 +0530

    multiple proxy added

--- zuul.server/src/main/java/zuul/server/ZuulserverApplication.java
+++ zuul.server/src/main/java/zuul/server/ZuulserverApplication.java
@@ -2,12 +2,10 @@ package zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class ZuulserverApplication {
 
 	public static void main(String[] args) {
